1557edf4a84786f3c9c37915aad0276f96d048f0
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-16 23:08:41 +0100

    typeText and generateUniqueId functions was created.

--- client/script.js
+++ client/script.js
@@ -7,3 +7,39 @@ const chatContainer = document.querySelector('#chat_container');
 
 
 let loadInterval;
+
+// * Functions that will load the messages
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      // will get character under a specific index, in the text that the AI is going to return. 
+      // element.innerHTML = element.innerHTML + text[index];
+      element.innerHTML += text.chartAt(index)
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 2000)
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  // will get 16 characters
+  const hexadecimalString = randomNumber.toString(16)
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
